c00ba5659208a36fbce0eda7a76141b5ff3853e6
ur5fot <wagvpered@gmail.com>
2015-11-16 22:34:02 +0200

    DOMContentLoaded

--- tooltip_clik.js
+++ tooltip_clik.js
@@ -1,9 +1,12 @@
 /**
  * Created by ur5fot on 12.11.15.
  */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', yogahotTooltip);
+
+function yogahotTooltip() {
     document.addEventListener('click', tooltipShow);
     var x, y, t, showTooltip;
+
     function tooltipShow(event) {
         t = event.target;
         y = event.pageY;
@@ -16,16 +19,16 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.appendChild(tooltipE);
             showTooltip = tooltipE;
 
-            if (tooltipE.offsetWidth + 10  < x) {
-                tooltipE.style.left = x - 10 - tooltipE.offsetWidth + 'px';
-            } else if (tooltipE.offsetWidth + 10 > x) {
-                tooltipE.style.left = x + 10 + 'px';
+            if (tooltipE.offsetWidth + 15 < x) {
+                tooltipE.style.left = x - 15 - tooltipE.offsetWidth + 'px';
+            } else if (tooltipE.offsetWidth + 15 > x) {
+                tooltipE.style.left = x + 15 + 'px';
             }
 
-            if (tooltipE.offsetHeight + 10  < y) {
-                tooltipE.style.top = y - 10 - tooltipE.offsetHeight + 'px';
-            } else if (tooltipE.offsetWidth + 10 >= y) {
-                tooltipE.style.top = y + 10 + 'px';
+            if (tooltipE.offsetHeight + 15 < y) {
+                tooltipE.style.top = y - 15 - tooltipE.offsetHeight + 'px';
+            } else if (tooltipE.offsetWidth + 15 >= y) {
+                tooltipE.style.top = y + 15 + 'px';
             }
 
         } else {
@@ -43,4 +46,4 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-});
+};
